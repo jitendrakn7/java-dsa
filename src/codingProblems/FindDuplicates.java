33b860e344cd699f6a7491bfabd66393656f595f
jitendrakn7 <153443570+jitendrakn7@users.noreply.github.com>
2025-08-01 22:52:30 +0530

    How will you find duplicate elements from a list of integers using streams

---
+++ src/codingProblems/FindDuplicates.java
@@ -0,0 +1,14 @@
+package codingProblems;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FindDuplicates {
+    public static void main(String[] args){
+        List<Integer> nums = Arrays.asList(1, 2, 3, 2, 4, 5, 1);
+        List duplicateNums = nums.stream().collect(Collectors.groupingBy(n-> n,Collectors.counting())).entrySet().stream().filter(v-> v.getValue()>1).map(Map.Entry::getKey).toList();
+        System.out.println(duplicateNums);
+    }
+}
